a22990d5618c15dbdabc193fd6b981f1dff771e2
Julio Alberto <julio.aranis.contreras@gmail.com>
2021-01-24 00:41:37 +0100

    fixed colors

--- components/template/footer.vue
+++ components/template/footer.vue
@@ -1,9 +1,9 @@
 <template>
   <div class=" flex-col text-center flex justify-center py-4 px-5 bg-quaternary">
-    <div class="text-primary-100">
+    <div class="text-primary-variant-100">
       This <a class="underline px-1" href="https://github.com/julioacontreras/portfolio">site</a> is powered by:
     </div>
-    <div class="text-primary-400">
+    <div class="text-primary-variant-400">
       NuxtJs + TailwindCSS + Unicorn UI Components ♥
     </div>
   </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,10 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: {
+        primary: '#3A0CA3',
+        'primary-variant': {
           400: '#af92f3',
-          100: '#5c27da',
-          default: '#3A0CA3'
+          100: '#5c27da'
         },
         secondary: '#F72585',
         ternary: '#444444',
